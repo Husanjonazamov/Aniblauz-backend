a0e86d4c07bee9cc8d18af83f0d59d659e743951
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-02-15 13:02:19 +0500

    Episode view: added listing by anime and updated URL routing

--- core/apps/anime/views/episode.py
+++ core/apps/anime/views/episode.py
@@ -5,8 +5,16 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from ..models import EpisodeModel
+from ..models import EpisodeModel, AnimeModel
 from ..serializers.episode import CreateEpisodeSerializer, ListEpisodeSerializer, RetrieveEpisodeSerializer
+
+
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
 
 
 @extend_schema(tags=["episode"])
@@ -21,3 +29,15 @@ class EpisodeView(BaseViewSetMixin, ReadOnlyModelViewSet):
         "retrieve": RetrieveEpisodeSerializer,
         "create": CreateEpisodeSerializer,
     }
+
+    @action(detail=False, methods=['get'], url_path='anime/(?P<anime_id>[^/.]+)')
+    def list_by_anime(self, request, anime_id=None):
+        anime = get_object_or_404(AnimeModel, id=anime_id)
+        episodes = EpisodeModel.objects.filter(anime=anime)
+        serializer = ListEpisodeSerializer(episodes, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return RetrieveEpisodeSerializer
+        return super().get_serializer_class()
--- core/apps/api/urls.py
+++ core/apps/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from core.apps.users.views import UserView
-from core.apps.anime.views import AnimeView
+from core.apps.anime.views import AnimeView, EpisodeView
 
 router = DefaultRouter()
 
 # anime url
 router.register(r"anime", AnimeView, basename='anime')
+router.register(r"episodes", EpisodeView, basename='episode')
+
 
 # users url
 router.register(r"users", UserView, basename='users')
